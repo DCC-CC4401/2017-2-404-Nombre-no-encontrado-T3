e7f7a457ade609a2563a012be86f18682d823d54
Gabriel Norambuena <gabo.norambuena@gmail.com>
2017-10-22 00:32:02 -0300

    fixed untracked files

--- app/municipal.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-from app.decorators import group_required
-
-@login_required()
-@group_required('municipal')
-def homeMunicipalidad(request):
-    return render(request,'muni-estadisticas-ongs.html')
