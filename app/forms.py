669b743084c0c9d3aa3bd308ea2b081d769a6622
mtorrealba <matias.torrealba.a@gmail.com>
2017-10-21 23:45:33 -0300

    Agregado vista denuncia, modelo y formulario

--- app/forms.py
+++ app/forms.py
@@ -38,6 +38,42 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
+class SignUpFormMunicipalUser(UserCreationForm):
+    first_name = forms.CharField(
+        label="Nombre de la Municipalidad",
+        max_length=30,
+        required=True,
+        help_text='Requerido')
+    last_name = forms.CharField(
+        label="Apellido",
+        max_length=30,
+        required=False,
+        help_text='Opcional.')
+    email = forms.EmailField(max_length=254, help_text='Obligatorio, ingresa una dirección de correo válida.')
+
+    error_messages = {
+        'password_mismatch': "Las dos contraseñas no son iguales.",
+    }
+
+    password1 = forms.CharField(
+        label="Contraseña",
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text="La contraseña debe contener al menos 8 caracteres. Su contraseña no puede ser una contraseña comúnmente utilizada\n ni tampoco enteramente numérica. ",
+    )
+    password2 = forms.CharField(
+        label="Confirmación Contraseña",
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text="Ingrese la misma contraseña para validar.",
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+
+
 
 class DenunciaForm(forms.Form):
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Denuncia(models.Model):
@@ -38,6 +41,7 @@ class Denuncia(models.Model):
     )
 
     estado = models.CharField(choices=ESTADO, max_length=15, default="RE")
+    estado = models.CharField(max_length=15)
     calle = models.CharField(max_length=50)
     comuna = models.CharField(max_length=20)
     herido = models.CharField(choices=HERIDO, max_length=15)
@@ -86,3 +90,18 @@ class Animal(models.Model):
     def __str__(self):
         return '%s %s' % (self.nombre, self.tipo)
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="img/userprofile")
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
